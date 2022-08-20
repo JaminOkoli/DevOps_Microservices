8955781c92a36ed9b9167f6f1fc7e0e9f7119b4d
EC2 Default User <ec2-user@ip-172-31-65-21.ec2.internal>
2022-08-20 10:22:53 +0000

    k8s changes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="lorenzo1100/serviceapi:v1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run serviceapi\
+    --image=$dockerpath\
+    --port=80 --labels app=serviceapi
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward serviceapi 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=lorenzo1100/serviceapi:v1.0
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker image tag serviceapi $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
